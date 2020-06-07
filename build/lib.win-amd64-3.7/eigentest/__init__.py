69395f66e50a9dc50a37525c1b23a73dfadbc6da
xliusufe <liuxuxm@163.com>
2020-06-08 00:26:10 +0800

    updated

--- build/lib.win-amd64-3.7/eigentest/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time    : May 1, 2020
-# @Author  : Xu Liu
-# @Site    : 'https://github.com/xliusufe/eigentest/'
-# @File    : __init__.py
-
-from .eigentest import generateMatrix
-__all__ = 'generateMatrix'
